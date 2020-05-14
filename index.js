e60f4d58b28a76c4125bb8728948b77f9c25f75a
AlexanderDV <dikunav@gmail.com>
2020-05-14 19:19:06 +0500

    v0.0.3a Add image changing Fix bugs Add saving Split on categories Add map Add element moving and hiding Add options Add debug Correct counters

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-//--- Name: AnimalProgJs/Vesion: 0.0.1a/Authors: AlexanderDV/Description: Main AnimalProgJs .js. ---
+//--- Name: AnimalProgJs/Vesion: 0.0.2a/Authors: AlexanderDV/Description: Main AnimalProgJs .js. ---
 var programInfo={
-	Name : "AnimalProgJs",
-	Version : "0.0.1a",
-	Authors : "AlexanderDV"
+	name : "AnimalProgJs",
+	version : "0.0.2a",
+	authors : "AlexanderDV"
 }
 programInfo.title= programInfo.name + " v" + programInfo.version + " by " + programInfo.authors
 document.title=programInfo.title
@@ -15,107 +15,19 @@ function setElementsTextTranslates(lang){
 }
 setElementsTextTranslates()
 
-var canvas={
-    masshtab:10
-}
-var entities={}
-function distance(x0,y0,x1,y1){
-    return Math.pow(Math.pow(x0-x1,2)+Math.pow(y0-y1,2),0.5)
-}
-function Entity(name,image,x,y,speed){
-    this.image=image
-    this.pos={
-        x:x,
-        y:y
-    }
-    this.speed=speed
-    this.moveAt=function(x,y){
-        this.pos.x=x
-        this.pos.y=y
-        this.image.style.left=this.pos.x+"px"
-        this.image.style.bottom=this.pos.y+"px"
-    }
-    this.move=function(x,y){
-        this.pos.x+=x
-        this.pos.y+=y
-        this.moveAt(this.pos.x,this.pos.y)
-    }
-    this.performing=[]
-    this.moveByTime=function(x,y,type,end){
-        var ent=this
-        var mbtid=setInterval(function()
-        {
-            if(end=="all"?allPerforming.length!=0:(end=="cur"?ent.performing.length!=0:false))
-                return
-            if(type=="add")
-            {
-                x+=ent.pos.x
-                y+=ent.pos.y
-            }
-            var startX=ent.pos.x
-            var startY=ent.pos.y
-            var startTime=new Date().getTime()
-            console.log(startX,startY,x,y);
-            var count=0
-            var iv=10
-            var dist=distance(x,y,startX,startY)
-            var xSpeed=ent.speed*(x-startX)/dist
-            var ySpeed=ent.speed*(y-startY)/dist
-            var lastX,lastY
-            lastX=x-ent.pos.x
-            lastY=y-ent.pos.y
-            var id=setInterval(function(){
-                if((Math.abs(x-ent.pos.x)>1||Math.abs(y-ent.pos.y)>1)&&Math.abs(lastX)>=Math.abs(x-ent.pos.x)&&Math.abs(lastY)>=Math.abs(y-ent.pos.y))
-                {
-                    lastX=x-ent.pos.x
-                    lastY=y-ent.pos.y
-                    ent.move(xSpeed/1000*iv*canvas.masshtab,ySpeed/1000*iv*canvas.masshtab)
-                }
-                else
-                {
-                    clearInterval(id)
-                    for(var v in ent.performing)
-                        if(ent.performing[v]==id)
-                            ent.performing.splice(v,1)
-                    for(var v in allPerforming)
-                        if(allPerforming[v]==id)
-                            allPerforming.splice(v,1)
-                }
-            },iv)
-            allPerforming.push(id)
-            ent.performing.push(id)
-            clearInterval(mbtid)
-        },100)
-    }
-    this.distanceTo=function(x,y)
-    {
-        return distance(this.pos.x,this.pos.y,x,y)
-    }
-    this.moveAt(x,y)
-    entities[name]=this
-}
-new Entity("dino",dinoDiv,0,0,10)
-new Entity("chest",chestDiv,500,500,0)
-entities.dino.moveAt(0,0)
-var maxTryes=3,maxDst=100
-var tryes=0,wins=0
+var maxDst=100
 var allPerforming=[]
-function setTryes(val){
-    tryes=val
-    tryesLabel.innerText=tryes+"/"+maxTryes
-}
-function setWins(val){
-    wins=val
-    winsLabel.innerText=wins
-}
-function newGame(){
-    setTryes(0)
-    setWins(wins)
-    entities.chest.moveAt(document.documentElement.clientWidth*Math.random(),document.documentElement.clientHeight*Math.random())
-}
 var cmds={}
 function initSettings(){
-    commandsArea.value=storageValue("commandsArea")||'{"двигайся":function(args){entities.dino.moveByTime(Number(args[1]),Number(args[2]),"","cur")}}'
+    commandsArea.value=storageValue("commandsArea")||`
+{
+	"двигайся_в" : args => {
+		entities.dino.moveByTime(Number(args[1]),Number(args[2]),"in","cur")
+	},
+	"двигайся_на" : args => {
+		entities.dino.moveByTime(Number(args[1]),Number(args[2]),"on","cur")
+	}
+}`
     eval("cmds="+commandsArea.value)
 }
 initSettings()
@@ -149,8 +61,118 @@ codeArea.onkeydown=function(e){
         return false
     }
 }
-newGame()
-performButton.onclick=function(){
+// Init mover-elements
+function initMovingElements(){
+	for(var v in document.getElementsByClassName("mover"))
+		addMovingElement(document.getElementsByClassName("mover")[v].parentNode, document.getElementsByClassName("mover")[v])
+}
+initMovingElements()
+var defaultStorables={
+	mapWidthOptionInput:500,
+	mapHeightOptionInput:500,
+}
+function initStorableElements(){
+	for(var v of document.getElementsByClassName("storable"))
+		((v)=>{
+			var el=v//document.getElementsByClassName("storable")[v]
+			console.log(el,v);
+			el[(el.type=="checkbox"||el.type=="radio")?"checked":"value"]=defaultStorables[el.id]!=undefined?(storageValue(el.id)||defaultStorables[el.id]):(storageValue(el.id)!=undefined?storageValue(el.id):el[(el.type=="checkbox"||el.type=="radio")?"checked":"value"])
+			var f=()=>storageValue(el.id,el[(el.type=="checkbox"||el.type=="radio")?"checked":"value"])
+			el.addEventListener('input',f)
+			el.addEventListener('change',f)
+			el.addEventListener('enter',f)
+			f()
+			if(el.onchange)
+				el.onchange()
+			if(el.oninput)
+				el.oninput()
+		})(v)
+}
+initStorableElements()
+var hidingEls={
+	optionsHider	:	"optionsGroupDiv",
+	controlHider	:	"controlGroupDiv",
+	countersHider	:	"countersGroupDiv",
+	mapHider		:	"mapGroupDiv",
+	instancesHider	:	"instancesGroupDiv",
+	debugHider		:	"debugGroupDiv",
+}
+for(var vv in hidingEls)
+	((vv)=>addHidingElement(eval(hidingEls[vv]),	eval(vv),	(v)=>storageValue(vv,v),	storageValue(vv)))(vv)
+//Counters
+var counters
+function clearCounters(){
+	counters={
+		currentTry:0,
+		tries:0,
+		victories:0,
+		defeats:0
+	}
+}
+clearCounters()
+var countersDivs={}
+var countersOptionsDivs={}
+var countersLabels={}
+var countersMax={
+	currentTry:3
+}
+function generateCounters(){
+	for(var name in counters)
+		(name=>{
+			countersDivs[name]=document.createElement('div')
+			countersDiv.appendChild(countersDivs[name])
+			var n=document.createElement('label')
+			n.innerText=getMsg(name)
+			countersDivs[name].appendChild(n)
+			var s=document.createElement('label')
+			s.innerText=": "
+			countersDivs[name].appendChild(s)
+			var c=document.createElement('label')
+			countersLabels[name]=c
+			countersDivs[name].appendChild(c)
+
+
+			countersOptionsDivs[name]=document.createElement('div')
+			countersOptionsDiv.appendChild(countersOptionsDivs[name])
+			var l=document.createElement('label')
+			l.innerText=getMsg(name)
+			countersOptionsDivs[name].appendChild(l)
+			var i=document.createElement('input')
+			i.type="checkbox"
+			i.checked=true
+			i.onchange=function(){
+				countersDivs[name].style.display=this.checked?"":"none"
+			}
+			countersOptionsDivs[name].appendChild(i)
+		})(name)
+}
+generateCounters()
+function updateCounters(){
+	for(var name in counters)
+		countersLabels[name].innerText=counters[name]+(countersMax[name]?"/"+countersMax[name]:"")
+}
+//Events
+function victory(){
+	alert(getMsg("victory"))
+	counters.victories++
+	newGame()
+}
+function defeat(){
+	alert(getMsg("defeat"))
+	counters.defeats++
+	newGame()
+}
+function newGame(){
+	counters.currentTry=0
+	if(randomNewGameDinoSpawnOptionCheckbox.checked)
+    	entities.dino.moveIn(mapSettings.max.x*Math.random(),mapSettings.max.y*Math.random())
+	if(randomNewGameChestSpawnOptionCheckbox.checked)
+    	entities.chest.moveIn(mapSettings.max.x*Math.random(),mapSettings.max.y*Math.random())
+	updateCounters()
+}
+function newTry(){
+	counters.currentTry++
+	counters.tries++
     if(languageSelect.value=="Commands")
     {
         var spl=codeArea.value.split("\n")
@@ -164,15 +186,157 @@ performButton.onclick=function(){
         if(allPerforming.length!=0)
             return
         console.log(entities.chest.distanceTo(entities.dino.pos.x,entities.dino.pos.y));
-        setTryes(tryes+1)
-        if(tryes>=maxTryes)
-            newGame()
         if(entities.chest.distanceTo(entities.dino.pos.x,entities.dino.pos.y)<maxDst)
-        {
-            alert("Win!")
-            wins++
-            newGame()
-        }
+			victory()
+		else if(counters.currentTry>=countersMax.currentTry)
+			defeat()
         clearInterval(ciid)
     },100)
+	updateCounters()
+}
+performButton.onclick=newTry
+//Entities
+var entities={}
+function updateEntities(){
+	for(var v in entities)
+		entities[v].moveIn(entities[v].pos.x,entities[v].pos.y)
+}
+var mapSettings
+function updateMapSettings(){
+	mapSettings={
+	    masshtab:10,
+		min	:	{
+			x	:	0,
+			y	:	0
+		},
+		max	:	{
+			x	:	mapDiv.getBoundingClientRect().width,
+			y	:	mapDiv.getBoundingClientRect().height
+		},
+		bottom	:	mapDiv.getBoundingClientRect().bottom,
+		left	:	mapDiv.getBoundingClientRect().left,
+		right	:	mapDiv.getBoundingClientRect().right,
+		top		:	mapDiv.getBoundingClientRect().top,
+	}
+	updateEntities()
+}
+function setStorableValue(poolPath,name,value,def,modif,link,linkV,update){
+	if(def)
+		value=storageValue(poolPath+"."+name)||value
+	eval(poolPath)[name]=value+modif
+	if(link)
+		link[linkV]=value
+	storageValue(poolPath+"."+name,value)
+	if(update)
+		update()
+}
+//setStorableValue("optionsGroupDiv.style","display","checked",true,"",optionsHider,"checked")
+//setStorableValue("mapDiv.style","width",500,true,"px",mapWidthOptionDiv,"value")
+//setStorableValue("mapDiv.style","height",500,true,"px",mapHeightOptionDiv,"value")
+//mapWidthOptionInput.value=storageValue("mapWidth")||500
+//mapWidthOptionInput.oninput()
+//mapHeightOptionInput.value=storageValue("mapHeight")||500
+//mapHeightOptionInput.oninput()
+updateMapSettings()
+var instances={}
+function instantiate(instance){
+	instances[instance.id]=instances[instance.id]||0
+	var n=document.createElement('div')
+	mapDiv.appendChild(n)
+	n.innerHTML=instance.innerHTML
+	n.id=instance.id+"Instance"+(instances[instance.id])
+	n.style.cssText=instance.style.cssText
+	n.className=instance.className
+	for(var v in instance.children)
+		n.children[v].id=instance.children[v].id+"Instance"+(instances[instance.children[v].id])
+	instances[instance.id]++
+	return n
+}
+function Entity(name,instance,x,y,speed){
+    this.image=instantiate(instance)
+    this.pos={x:x,y:y}
+    this.speed=speed
+    this.moveIn=function(x,y){
+        this.pos	=	{
+			x:ogr(mapSettings.min.x,	x,	mapDiv.getBoundingClientRect().width	-this.image.getBoundingClientRect().width),
+			y:ogr(mapSettings.min.y,	y,	mapDiv.getBoundingClientRect().height	-this.image.getBoundingClientRect().height)
+		}
+		var offset=0
+		for(var v in entities)
+			if(v!=this.name)
+				offset+=entities[v].image.getBoundingClientRect().height
+
+        this.image.style.left	=	this.pos.x+"px"
+        this.image.style.top	=	mapDiv.getBoundingClientRect().height-this.pos.y-this.image.getBoundingClientRect().height-offset+"px"
+    }
+    this.moveOn=(x,y)=>this.moveIn(this.pos.x+x,this.pos.y+y)
+    this.performing=[]
+    this.moveByTime=function(x,y,type,end){
+        var ent=this
+        var mbtid=setInterval(function()
+        {
+            if(end=="all"?allPerforming.length!=0:(end=="cur"?ent.performing.length!=0:false))
+                return
+            if(type=="on")
+            {
+                x+=ent.pos.x
+                y+=ent.pos.y
+            }
+            var startX=ent.pos.x
+            var startY=ent.pos.y
+            var startTime=new Date().getTime()
+            console.log(startX,startY,x,y);
+            var count=0
+            var iv=10
+            var dist=distance(x,y,startX,startY)
+            var xSpeed=ent.speed*(x-startX)/dist
+            var ySpeed=ent.speed*(y-startY)/dist
+            var lastX,lastY
+            lastX=x-ent.pos.x
+            lastY=y-ent.pos.y
+            var id=setInterval(function(){
+                if((Math.abs(x-ent.pos.x)>1||Math.abs(y-ent.pos.y)>1)&&Math.abs(lastX)>=Math.abs(x-ent.pos.x)&&Math.abs(lastY)>=Math.abs(y-ent.pos.y))
+                {
+                    lastX=x-ent.pos.x
+                    lastY=y-ent.pos.y
+                    ent.moveOn(xSpeed/1000*iv*mapSettings.masshtab,ySpeed/1000*iv*mapSettings.masshtab)
+                }
+                else
+                {
+                    clearInterval(id)
+                    for(var v in ent.performing)
+                        if(ent.performing[v]==id)
+                            ent.performing.splice(v,1)
+                    for(var v in allPerforming)
+                        if(allPerforming[v]==id)
+                            allPerforming.splice(v,1)
+                }
+            },iv)
+            allPerforming.push(id)
+            ent.performing.push(id)
+            clearInterval(mbtid)
+        },100)
+    }
+    this.distanceTo=(x,y)=>distance(this.pos.x,this.pos.y,x,y)
+    this.moveIn(x,y)
+	this.name=name
+    entities[name]=this
+}
+function generateEntities()
+{
+	entities={}
+	mapDiv.innerHTML=""
+	new Entity("dino",dinoDiv,0,0,10)
+	new Entity("chest",chestDiv,500,500,0)
+	console.log(randomStartDinoSpawnOptionCheckbox.checked);
+	if(randomStartDinoSpawnOptionCheckbox.checked)
+    	entities.dino.moveIn(mapSettings.max.x*Math.random(),mapSettings.max.y*Math.random())
+	if(randomStartChestSpawnOptionCheckbox.checked)
+    	entities.chest.moveIn(mapSettings.max.x*Math.random(),mapSettings.max.y*Math.random())
+}
+//Start
+startButton.onclick=()=>{
+	generateEntities()
+	clearCounters()
+	updateCounters()
 }
--- properties.js
+++ properties.js
@@ -1,21 +1,71 @@
-/*--- Name: AnimalProgJs/Vesion: 0.0.1a/Authors: AlexanderDV/Description: Properties AnimalProgJs .js. ---*/
+/*--- Name: AnimalProgJs/Vesion: 0.0.2a/Authors: AlexanderDV/Description: Properties AnimalProgJs .js. ---*/
 //Properties
 var props	=	{}
 props.msgs  = {
-    en  : {
-        "perform":"Perform",
-        "language":"Language",
-        "setCommands":"Set commands",
-        "tryes":"Tryes",
-        "wins":"Wins"
-    },
-    ru  : {
-        "perform":"Выполнить",
-        "language":"Язык",
-        "setCommands":"Установить команды",
-        "tryes":"Попытки",
-        "wins":"Победы"
-    }
+	en  : {
+		//Part
+			"options"	:	"Options",
+			"control"	:	"Control",
+			"counters"	:	"Counters",
+			"map"		:	"Map",
+			"instances"	:	"Instances",
+			"debug"		:	"Debug",
+		//Options
+			//Spawn
+				"randomStartDinoSpawn"		:	"Spawn Dino random on start",
+				"randomStartChestSpawn"		:	"Spawn Chest random on new game",
+				"randomNewGameDinoSpawn"	:	"Spawn Dino random on start",
+				"randomNewGameChestSpawn"	:	"Spawn Chest random on new game",
+			//Map
+				"mapwidth"	:	"Map width",
+				"mapHeight"	:	"Map height",
+		//Control
+			"perform"		:	"Perform",
+			"language"		:	"Language",
+			"setCommands"	:	"Set commands",
+		//Counter
+			"currentTry":	"Current try",
+			"tries"		:	"Tries",
+			"victories"	:	"Victories",
+			"defeats"	:	"Defeats",
+		//Messages
+			"victory"	:	"Victory",
+			"defeat"	:	"Defeat",
+		//Buttons
+			"start"	:	"Start",
+	},
+	ru  : {
+		//Part
+			"options"	:	"Настройки",
+			"control"	:	"Управление",
+			"counters"	:	"Счетчики",
+			"map"		:	"Карта",
+			"instances"	:	"Образцы",
+			"debug"		:	"Дебаг",
+		//Options
+			//Spawn
+				"randomStartDinoSpawn"		:	"Спавнить Дино случайно в начале",
+				"randomStartChestSpawn"		:	"Спавнить Сундук случайно в начале",
+				"randomNewGameDinoSpawn"	:	"Спавнить Дино случайно в новой игре",
+				"randomNewGameChestSpawn"	:	"Спавнить Сундук случайно в новой игре",
+			//Map
+				"mapWidth"	:	"Ширина карты",
+				"mapHeight"	:	"Высота карты",
+		//Control
+			"perform"		:	"Выполнить",
+			"language"		:	"Язык",
+			"setCommands"	:	"Установить команды",
+		//Counter
+			"currentTry":	"Данная попытка",
+			"tries"		:	"Попыток",
+			"victories"	:	"Побед",
+			"defeats"	:	"Поражений",
+		//Messages
+			"victory"	:	"Победа",
+			"defeat"	:	"Поражение",
+		//Buttons
+			"start"	:	"Начать",
+	}
 }
 
 // Universal local storage initialization
